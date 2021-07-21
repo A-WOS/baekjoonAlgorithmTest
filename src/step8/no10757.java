66b52105feeda7316de335794298e0d657495eaf
Awos <uws0331@gmail.com>
2021-07-21 22:02:34 +0900

    백준 10757번 큰 수 A+B

---
+++ src/step8/no10757.java
@@ -0,0 +1,15 @@
+package step8;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class no10757 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.println(A.add(B));
+    }
+}
