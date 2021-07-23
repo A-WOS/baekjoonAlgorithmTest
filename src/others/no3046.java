0c0ad8353d821ea9b9ba0460662a364454dc66c2
Awos <uws0331@gmail.com>
2021-07-23 21:48:27 +0900

    백준 3046번 R2

---
+++ src/others/no3046.java
@@ -0,0 +1,15 @@
+package others;
+
+import java.util.Scanner;
+
+// 백준 3046번 R2
+public class no3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        System.out.println(S*2-R1);
+
+    }
+}
