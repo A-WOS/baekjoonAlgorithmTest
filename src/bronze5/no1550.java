e488e677ba28efbff43f2af6607ffdc1ee7746d8
Awos <uws0331@gmail.com>
2021-08-03 22:25:55 +0900

    백준 1550번 16진수

---
+++ src/bronze5/no1550.java
@@ -0,0 +1,12 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class no1550 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int hexToTen = Integer.parseInt(s, 16);
+        System.out.println(hexToTen);
+    }
+}
