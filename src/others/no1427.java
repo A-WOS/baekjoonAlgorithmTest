8bf85e634f29d757f8c6d83a310e98601d30a0d7
Awos <uws0331@gmail.com>
2021-08-07 00:21:44 +0900

    1427 테스트완료

--- src/others/no1427.java
+++ src/others/no1427.java
@@ -1,6 +1,7 @@
 package others;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class no1427 {
@@ -10,19 +11,21 @@ public class no1427 {
 
         int[] arr = new int[10];
 
+        int count=0;
         while(N != 0){
-//            arr[N%10] = N%10;
             arr[N%10]++;
             N = N/10;
+            count++;
 
         }
-
-//        Arrays.sort(arr);
+        System.out.println(count);
 
         for(int j=arr.length -1; j>=0; j--){
             while(arr[j]-- > 0)
                 System.out.print(j);
         }
+
+
     }
 
 }
