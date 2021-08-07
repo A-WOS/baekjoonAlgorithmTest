aa67b73e6c06863361d9f7899d0395c9bbf895ef
Awos <uws0331@gmail.com>
2021-08-07 13:11:39 +0900

    백준 2338번 긴자리 계산

---
+++ src/bronze5/no2338.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class no2338 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
+
+    }
+}
