0425ebd541372e515301dc1db7d24c2312449b57
Awos <uws0331@gmail.com>
2021-08-11 17:39:28 +0900

    백준 2501번 약수 구하기

--- src/bronze3/no2501.java
+++ src/bronze3/no2501.java
@@ -18,7 +18,6 @@ public class no2501 {
         for(int i=0; i<N; i++){
             if (N % (i+1) == 0){
                 arr[i] = i+1;
-                System.out.println(arr[i]);
                 n_count++;
             }
         }
